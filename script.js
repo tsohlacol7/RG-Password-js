74760307d51a636ddc9598bdf0310580ff86db69
GIorgos <giorgos@pop-os.localdomain>
2024-03-23 19:33:43 +0200

    rgp

---
+++ script.js
@@ -0,0 +1,30 @@
+const passwordBox = document.getElementById("password");
+const length =  16;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase ="abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "@#$%^&*()_+~|}{[]></-=";
+
+const allCharacters = upperCase + lowerCase + number + symbols;
+
+//Create password function 
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length) ];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length) ];
+    password += number[Math.floor(Math.random() * number.length) ];
+    password += symbols[Math.floor(Math.random() * symbols.length) ];
+
+    while(length > password.length){
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length) ];
+    }
+    passwordBox.value = password;
+}
+
+//copy function
+
+function copyPassword(){
+    navigator.clipboard.writeText(passwordBox.value);
+}
+
